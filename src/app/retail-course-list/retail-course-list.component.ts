46985f3f1409c774e43bf8c5f9586fb74564b8ea
sangeetha <sangithamanoj@gmail.com>
2021-08-03 12:23:30 +0530

    Retail course dispaly code for ltt and mst

--- src/app/retail-course-list/retail-course-list.component.ts
+++ src/app/retail-course-list/retail-course-list.component.ts
@@ -9,6 +9,44 @@ import { CourseServiceService } from '../course-service.service';
   styleUrls: ['./retail-course-list.component.css']
 })
 export class RetailCourseListComponent implements OnInit {
+ltt=[{name:'',
+category:'',
+about:'',
+objective:'',
+sponserimage:'',
+knowledgeParterimage:'',
+internshipPartnerimage:'',
+courseDelivery:'',
+agenda:'',
+highlights:'',
+eligibility:'',
+test:'',
+courseFee:'',
+refundPolicy:'',
+importantDates:'',
+img1:'',
+img2:'',
+questionPaperLink:'',
+status:''}];
+mst=[{name:'',
+category:'',
+about:'',
+objective:'',
+sponserimage:'',
+knowledgeParterimage:'',
+internshipPartnerimage:'',
+courseDelivery:'',
+agenda:'',
+highlights:'',
+eligibility:'',
+test:'',
+courseFee:'',
+refundPolicy:'',
+importantDates:'',
+img1:'',
+img2:'',
+questionPaperLink:'',
+status:''}];
 
 
   data=[{
@@ -43,6 +81,21 @@ export class RetailCourseListComponent implements OnInit {
    
   this._courseService.getRetailCourses().subscribe((data:any)=>{
     this.data=JSON.parse(JSON.stringify(data));
+ this.ltt.length=0;
+ this.mst.length=0;
+    for (var i in this.data) {
+         if(this.data[i].category==='Long-Term Training(LTT)')
+         {
+           this.ltt.push(this.data[i]);
+         }
+         else if(this.data[i].category==='Micro skill Training(MST)'){
+          this.mst.push(this.data[i]);
+         }
+      
+    }
+
+
+
   });
   }
   getDetails(name:any){
